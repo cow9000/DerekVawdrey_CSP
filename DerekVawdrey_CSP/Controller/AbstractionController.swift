cd6be19cb1ab10bb33bc71ca493f98693d798123
Derek Vawdrey <flyingpiechicken@gmail.com>
2017-11-27 12:34:38 -0700

    Added the rest of the code to AbstractionController

--- DerekVawdrey_CSP/Controller/AbstractionController.swift
+++ DerekVawdrey_CSP/Controller/AbstractionController.swift
@@ -25,18 +25,57 @@ class AbstractionController: UIPageViewController, UIPageViewControllerDataSourc
     
     //Helper method to retrieve the correctViewController
     private func newAbstractionViewController(abstractionLevel: String) -> UIViewController{
-        
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "\(abstractionLevel)ViewController")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        dataSource = self
+        
+        if let firstViewController = orderedAbstractionViews.first
+        {
+            setViewControllers([firstViewController], direction: .forward, animated:true, completion:nil)
+        }
         // Do any additional setup after loading the view.
     }
 
     
-    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController:UIViewController) -> UIViewController{
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore
+        viewController:UIViewController) -> UIViewController?{
+        guard let viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+        else
+        {
+            return nil
+        }
+        let previousIndex = viewControllerIndex - 1
+        guard previousIndex >= 0
+        else{
+            return orderedAbstractionViews.last
+        }
+        guard orderedAbstractionViews.count > previousIndex
+        else{
+            return nil
+        }
         
+        return orderedAbstractionViews[previousIndex]
+    }
+    
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController:UIViewController) ->UIViewController?{
+        guard let viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+        else
+        {
+            return nil
+        }
+        let nextIndex = viewControllerIndex + 1
+        guard nextIndex >= 0
+        else{
+                return nil
+        }
+        guard nextIndex < orderedAbstractionViews.count
+        else{
+           return orderedAbstractionViews.first
+        }
+        return orderedAbstractionViews[nextIndex]
     }
     
     override func didReceiveMemoryWarning() {
