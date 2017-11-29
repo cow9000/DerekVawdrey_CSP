f6823440ad116abf450e4412aa91febd58c6635d
Derek Vawdrey <flyingpiechicken@gmail.com>
2017-11-29 11:52:51 -0700

    Added framework for InternetController

--- DerekVawdrey_CSP/Controller/InternetController.swift
+++ DerekVawdrey_CSP/Controller/InternetController.swift
@@ -8,28 +8,55 @@
 
 import UIKit
 
-class InternetController: UIViewController {
-
+class InternetController: UITableViewController {
+    
+    private (set) lazy var internetTopics : [String] =
+    {
+       return [
+            "Definitions",
+            "CSP",
+            "CTEC",
+            "Canyons",
+            "Twitter",
+            "Swift Guide"
+        ]
+    }()
+    
+    private var detailViewController : InternetDetailViewController?
+    
+    private func setup() -> Void{
+        
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setup()
         // Do any additional setup after loading the view.
+        self.clearsSelectionOnViewWillAppear = false
+    }
+    
+    // MARK: - Table view data source
+    override public func numberOfSections(in tableView: UITableView) -> Int{
+        // #warning Incomplete - return the number of sections
+        return 1
+    }
+    
+    override public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return internetTopics.count
+    }
+    
+    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let currentText = internetTopics[indexPath.row]
+        cell.textLabel!.text = currentText
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
