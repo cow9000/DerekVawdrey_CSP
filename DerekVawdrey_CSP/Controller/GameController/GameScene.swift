be52964a6c6591fc2c2f034c0d0e657309ada9aa
Derek Vawdrey <flyingpiechicken@gmail.com>
2017-12-19 13:28:47 -0700

    Added Invader laser code

--- DerekVawdrey_CSP/Controller/GameController/GameScene.swift
+++ DerekVawdrey_CSP/Controller/GameController/GameScene.swift
@@ -107,22 +107,48 @@ public class GameScene: SKScene, SKPhysicsContactDelegate
     
     private func invokeInvaderFire() -> Void
     {
-        
+        let fireBullet = SKAction.run() {
+            self.fireInvaderBullet()
+        }
+        let waitToFireInvaderBullet = SKAction.wait(forDuration: 2.5)
+        let invaderFire = SKAction.sequence([fireBullet,waitToFireInvaderBullet])
+        let repeatForeverAction = SKAction.repeatForever(invaderFire)
+        run(repeatForeverAction)
     }
     
     func fireInvaderBullet() -> Void
     {
+        if(invadersThatCanFire.isEmpty){
+            gameLevel += 1
+            levelComplete()
+        }
         
+        if let randomInvader = invadersThatCanFire.randomElement()
+        {
+            randomInvader.fireBullet(scene: self)
+        }
     }
     
     func newGame() -> Void
     {
-        
+        let newGameScene = StartScene(size:size)
+        newGameScene.scaleMode = scaleMode
+        let transitionType = SKTransition.flipHorizontal(withDuration: 0.5)
+        view?.presentScene(newGameScene, transition: transitionType)
     }
     
     func levelComplete() -> Void
     {
-        
+        if(gameLevel <= maxLevels){
+            let levelCompleteScene = LevelCompleteScene(size:size)
+            levelCompleteScene.scaleMode = scaleMode
+            let transitionType = SKTransition.flipHorizontal(withDuration: 0.5)
+            view?.presentScene(levelCompleteScene, transition: transitionType)
+        }
+        else{
+            gameLevel = 1
+            newGame()
+        }
     }
     
     
@@ -156,7 +182,7 @@ public class GameScene: SKScene, SKPhysicsContactDelegate
     
     override public func didSimulatePhysics()
     {
-        
+        player.physicsBody?.velocity = CGVector(dx:accelerationX * 600, dy: 0)
     }
     
     //MARK:- Handle Motion
@@ -171,6 +197,9 @@ public class GameScene: SKScene, SKPhysicsContactDelegate
                 self.accelerationX = CGFloat(acceleration.x)
         } )
     }
+    
+    
+    
     
     
     //MARK:- SKPhysicsContactDelegate method
